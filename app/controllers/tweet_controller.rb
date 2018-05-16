52ecef17c7637c3e058e8bc4e140bebe7f6506f5
ccpomposo <jesterc3@gmail.com>
2018-05-15 22:22:30 -0500

    Consumo de la API de Twitter

---
+++ app/controllers/tweet_controller.rb
@@ -0,0 +1,35 @@
+require 'twitter'
+class TweetController < ApplicationController
+    def index
+        @array
+        search = params[:search]
+        config = {
+            consumer_key:    "El5Kg9c8SR4CDvBNdlNkE9G1Q",
+            consumer_secret: "xJaJxUTvkAXmXfx9ZpHrfQq8r2HmqqivbzTyRXNj9aVUyW0L4R",
+        }
+        if search 
+            client = Twitter::REST::Client.new(config)
+            tweets = client.search(search.to_s, result_type: "recent")
+            @array = tweets.map do |t|
+                        t if t.coordinates != "null"
+                    end
+            @array = @array.compact 
+            @array = tweets.map{ |t| t if t.coordinates != "null"}.compact
+            @array.each do |tweet|
+                puts tweet.text
+            end
+            @size = @array.length
+        else
+
+        end
+        respond_to do |format|
+            format.html
+            format.json{
+              render json: @array
+            }
+        end
+    end
+
+    def search
+    end
+end
