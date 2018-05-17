fbc3ff515e813a3e7f2dd9d6eba8089d6324aa09
JavierAvendanoJmz <j4v13r117@hotmail.com>
2018-05-16 20:52:29 -0500

    Rutas, puntos sin pintar y algunos nulos

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -3,21 +3,19 @@ class TweetController < ApplicationController
     def index
         @array
         search = params[:search]
+        puts "@@@@@@@@@@@@@@@@"
+        puts search
         config = {
             consumer_key:    "El5Kg9c8SR4CDvBNdlNkE9G1Q",
             consumer_secret: "xJaJxUTvkAXmXfx9ZpHrfQq8r2HmqqivbzTyRXNj9aVUyW0L4R",
         }
         if search 
             client = Twitter::REST::Client.new(config)
-            tweets = client.search(search.to_s, result_type: "recent")
-            @array = tweets.map do |t|
-                        t if t.coordinates != "null"
-                    end
-            @array = @array.compact 
-            @array = tweets.map{ |t| t if t.coordinates != "null"}.compact
-            @array.each do |tweet|
-                puts tweet.text
-            end
+            tweets = client.search(search.to_s, result_type: "recent").take(100)
+            @array = tweets.map{ |t| t.to_json if t.to_json["coordinates"] != "null"}.compact
+            # @array.each do |tweet|
+            #     puts tweet.text
+            # end
             @size = @array.length
         else
 
@@ -31,5 +29,6 @@ class TweetController < ApplicationController
     end
 
     def search
+        
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tweet/search'
+  root 'tweet#search'
+
+  get 'tweet/index' => 'tweet#index'
+ 
 end
